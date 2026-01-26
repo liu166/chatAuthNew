d564175ac16ed33988ede35e117074be11a36200
coolBoy <291816874@qq.com>
2026-01-26 17:46:07 +0800

    commit

--- src/main/java/com/rag/auth/ChatAuthNewApplication.java
+++ src/main/java/com/rag/auth/ChatAuthNewApplication.java
@@ -1,9 +1,11 @@
 package com.rag.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rag.auth.dao")
 public class ChatAuthNewApplication {
 
     public static void main(String[] args) {
